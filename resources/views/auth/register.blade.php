e99eb19e25e7c83c5145b1b2459642c9b3eeafd6
Guillermo Mateo Carbajal <gtemateoc@gmail.com>
2024-06-25 15:47:27 -0500

    feat(Welcome,register,app):add_page_welcome_and_register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Regístrate en DevStagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex">
+        <div class="md:w-1/2">
+            <p>Imagen aquí</p>
+        </div>
+        <div class="md:w-1/2">
+            <form>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Name
+                    </label>
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Nombre de usuario"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Email de Registro"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password de Registro"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Password
+                    </label>
+                    <input
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Repite tu Password"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <input
+                    type="submit"
+                    value="Crear Cuenta"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                >
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./src/**/*.{html,js}"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
